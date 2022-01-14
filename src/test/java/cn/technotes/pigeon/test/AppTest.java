405fe1f32cccb35f243bb4cee01ed428d11b8d7a
duanjiliang <yuuyoo@163.com>
2022-01-14 16:37:25 +0800

    简单http服务器

---
+++ src/test/java/cn/technotes/pigeon/test/AppTest.java
@@ -0,0 +1,14 @@
+package cn.technotes.pigeon.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+	}
+
+}
