ce9ccc4d7fe25d623f1437f8c0b3d308d36be0fd
duanjiliang <yuuyoo@163.com>
2022-01-14 18:20:14 +0800

    增加随机端口功能

--- src/main/java/cn/technotes/pigeon/App.java
+++ src/main/java/cn/technotes/pigeon/App.java
@@ -6,11 +6,19 @@ import java.net.InetSocketAddress;
 import com.sun.net.httpserver.HttpServer;
 
 import cn.technotes.pigeon.core.RootHandler;
+import cn.technotes.pigeon.util.SocketUtils;
 
 public class App {
 
 	public static void main(String[] args) {
 		int port = 9090;
+
+		boolean isBind = SocketUtils.isBind(port);
+		if (isBind) {
+			System.out.println("端口 port : " + port + " 已占用");
+			port = SocketUtils.findAvaliablePort();
+		}
+
 		InetSocketAddress address = new InetSocketAddress(port);
 		// 允许排队的最大TCP连接数，如果该值小于或等于零，则使用系统默认值
 		int num = 0;
@@ -19,6 +27,8 @@ public class App {
 			server.createContext("/", new RootHandler());
 			server.setExecutor(null);
 			server.start();
+
+			System.out.println("服务已启动，监听端口：" + port);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
