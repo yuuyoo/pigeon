3f90f9bc6f9d59cd6fa2098deb8ae835236b7d30
duanjiliang <yuuyoo@163.com>
2022-01-17 15:04:49 +0800

    支持文件下载

--- src/main/java/cn/technotes/pigeon/core/RootHandler.java
+++ src/main/java/cn/technotes/pigeon/core/RootHandler.java
@@ -1,48 +1,96 @@
 package cn.technotes.pigeon.core;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.URI;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 
+import cn.technotes.pigeon.Constants;
+
 public class RootHandler implements HttpHandler {
 
+	public static final String RESOURCE_ROOT_DIRECTOR = new File("").getAbsolutePath();
+
 	@Override
-	public void handle(HttpExchange exchange) throws IOException {
+	public void handle(HttpExchange exchange) throws UnsupportedEncodingException {
+		URI uri = exchange.getRequestURI();
+		String path = uri.getPath();
+		String resource = RESOURCE_ROOT_DIRECTOR + path;
+		File file = new File(resource);
 
-		File directory = new File("");
-		String path = directory.getCanonicalPath();
-		String pathName = path.substring(path.lastIndexOf("\\") + 1);
+		if (file.isFile()) {
+			fileHandler(exchange, file);
+		}
 
+		if (file.isDirectory()) {
+			directoryHandler(exchange, path, file);
+		}
+	}
+
+	private void directoryHandler(HttpExchange exchange, String path, File directory) {
 		StringBuilder builder = new StringBuilder();
 		builder.append("<!DOCTYPE html>");
 		builder.append("<html>");
-		builder.append("<head><title>Index of /" + pathName + "/</title></head>");
+		builder.append("<head>");
+		builder.append("<meta http-equiv=\"Content-Type\" content=\"text/html\"; charset=\"utf-8\">");
+		builder.append("<title>Index of " + path + "</title>");
+		builder.append("</head>");
 		builder.append("<body bgcolor=\"white\">");
-		builder.append("<h1>Index of /" + pathName + "/</h1><hr><pre><a href=\"../\">../</a></br>");
+		builder.append("<h1>Index of " + path + "</h1><hr><pre><a href=\"../\">../</a></br>");
 
-		File[] files = directory.getCanonicalFile().listFiles();
-		for (File file : files) {
-			String filePath = file.getCanonicalPath();
-			String name = filePath.substring(filePath.lastIndexOf("\\") + 1);
-			builder.append("<a href=\"" + name + "/\">" + name + "/</a></br>");
-		}
+		try (OutputStream os = exchange.getResponseBody()) {
+			File[] files = directory.getCanonicalFile().listFiles();
+			for (File file : files) {
+				String filePath = file.getCanonicalPath();
+				String name = filePath.substring(filePath.lastIndexOf("\\") + 1);
 
-		builder.append("</pre><hr></body>");
-		builder.append("</html>");
-		String response = builder.toString();
-		exchange.getResponseHeaders().add("Content-Type:", "text/html;charset=utf-8");
-		exchange.sendResponseHeaders(200, response.length());
-		OutputStream os = null;
-		try {
-			os = exchange.getResponseBody();
-			os.write(response.getBytes());
+				// 过滤隐藏文件
+				if (name.startsWith(Constants.DOT)) {
+					continue;
+				}
+				name = URLDecoder.decode(name, "UTF-8");
+
+				builder.append("<a href=\"" + URLEncoder.encode(name, "UTF-8") + "\">" + name);
+				if (file.isDirectory()) {
+					builder.append(Constants.SLASH);
+				}
+				builder.append("</a></br>");
+			}
+
+			builder.append("</pre><hr></body>");
+			builder.append("</html>");
+			String response = builder.toString();
+			exchange.getResponseHeaders().add("Content-Type:", "text/html;charset=utf-8");
+
+			byte[] bs = response.getBytes();
+			exchange.sendResponseHeaders(200, bs.length);
+			os.write(bs);
+			return;
 		} catch (IOException e) {
-			System.out.println("root handler wirte reponse error:" + e.getMessage());
-		} finally {
-			os.close();
+			e.printStackTrace();
+		}
+	}
+
+	private void fileHandler(HttpExchange exchange, File file) {
+		try (FileInputStream inputStream = new FileInputStream(file); OutputStream os = exchange.getResponseBody();) {
+			exchange.getResponseHeaders().add("Content-Disposition", "attachment;filename=" + file.getName());
+			exchange.sendResponseHeaders(200, file.length());
+			byte[] fileBytes = new byte[(int) file.length()];
+			inputStream.read(fileBytes);
+			os.write(fileBytes);
+			return;
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 	}
 
